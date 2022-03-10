fdf58f1439febfa86a6f1b29435a20dd0abfe888
ju4ncito <villarrealjm2@gmail.com>
2022-03-10 14:32:18 -0300

    revente todo

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -1,49 +1,9 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
-import Link from 'next/link'
-import Date from '../components/date'
 
-import { getSortedPostsData } from "../lib/posts";
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
-export default function Home({ allPostsData }) {
+export default function Home({}) {
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          Heaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaalo, I'm <b>Juan</b>. I'm a software engineer student, currently
-          working as a <b>blockchain developer</b>. You can contact me on{" "}
-          <a href="https://twitter.com/0xJu4ncito">my Twitter</a>.
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-            <Link href={`/posts/${id}`}>
-              <a>{title}</a>
-            </Link>
-            <br />
-            <small className={utilStyles.lightText}>
-              <Date dateString={date} />
-            </small>
-          </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
+    <Head>
+      <title>TinyScoutsNFT</title>
+    </Head>
   );
 }
