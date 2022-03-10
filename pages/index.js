7314f034aa44b605ff6e3207f90c4b4976b5b027
ju4ncito <villarrealjm2@gmail.com>
2022-03-10 15:01:52 -0300

    initial tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Head from "next/head";
 import Link from "next/link";
+import { Navbar } from "../components/navbar";
 
 export default function Home() {
   return (
-    <Head>
-      <title>Tiny Scouts NFT</title>
-    </Head>
+    <div className="bg-green-200 h-screen">
+      <Head>
+        <title>Tiny Scouts NFT</title>
+      </Head>
+      <Navbar/>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
   );
 }
