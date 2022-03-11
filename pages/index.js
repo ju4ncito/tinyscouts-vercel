2f3ec5260677f66504e32853020342b6816d5c78
ju4ncito <villarrealjm2@gmail.com>
2022-03-11 09:19:06 -0300

    base

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
 import Head from "next/head";
 import Link from "next/link";
-import { Navbar } from "../components/navbar";
+
 
 export default function Home() {
   return (
     <div className="bg-green-200 h-screen">
       <Head>
-        <title>Tiny Scouts NFT</title>
+        <title>Utopian Lemurs</title>
       </Head>
-      <Navbar/>
+
+
+      <section className="">
+        <p>
+          Welcome to the Utopian Lemurs Official Website.
+        </p>
+      </section>
 
     </div>
   );
--- pages/roadmap.js
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-
-export default function Home({}) {
-  return (
-    <Head>
-      <title>TinyScoutsNFT</title>
-    </Head>
-  );
-}
