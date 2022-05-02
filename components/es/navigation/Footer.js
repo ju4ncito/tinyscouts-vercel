9c110e55e0be4c5a88c9750ae7e24c683fbcf23b
ju4ncito <villarrealjm2@gmail.com>
2022-05-02 19:47:18 -0300

    cambiado el footer, listo para reventar

--- components/es/navigation/Footer.js
+++ components/es/navigation/Footer.js
@@ -1,27 +1,30 @@
+
 export default function Footer() {
-    return (
-      <footer
-        className='bg-gray-50 dark:bg-black border-t-2'
-        aria-labelledby='footerHeading'
-      >
-        <h2 id='footerHeading' className='sr-only'>
-          Footer
-        </h2>
-        <div className='max-w-7xl mx-auto py-2 px-4'>
-          <div className='border-black dark:border-white flex items-center justify-between '>
-            <p className='my-8 text-base text-gray-600 dark:text-white leading-tight'>
-              Dreamurs Collection &copy; 
-              <br />
-              <span className='hidden sm:inline-block'>All Rights Reserved.</span>
-            </p>
-            <a
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-            </a>
-          </div>
+  return (
+    <footer
+      className='bg-gray-50 dark:bg-black border-t-2'
+      aria-labelledby='footerHeading'
+    >
+      <h2 id='footerHeading' className='sr-only'>
+        Footer
+      </h2>
+      <div className='max-w-7xl mx-auto py-2 px-4'>
+        <div className='border-black dark:border-white flex items-center justify-between '>
+          <p className='my-8 text-base text-gray-600 dark:text-white leading-tight'>
+            Made with love from the Tiny Scouts team &copy;2022
+            <br />
+
+          </p>
+          <a
+            href='https://twitter.com/TinyScoutsNFT'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <img src='/images/white-text.png' className='h-16' />
+          </a>
         </div>
-      </footer>
-    );
-  }
+      </div>
+    </footer>
+  );
+}
   
--- pages/en/index.js
+++ pages/en/index.js
@@ -26,9 +26,8 @@ export default function Home() {
 
 
       
-      {/* <footer>
-        <Footer />
-      </footer> */}
+     <Footer/>
+ 
     </>
   );
 }
